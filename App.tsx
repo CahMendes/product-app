da3cc74cb5232ae56aba54318e116adf3e082705
CahMendes <cah.mendes35@gmail.com>
2021-08-01 18:19:00 -0300

    criando toolbar

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { NativeBaseProvider, Box } from 'native-base';
+import { VStack, HStack, Button, IconButton, Icon, Text, Center, StatusBar } from "native-base";
+import { MaterialIcons, Fontisto, AntDesign } from '@expo/vector-icons';
+
+function AppBar() {
+  return (
+    <>
+      <StatusBar backgroundColor="#c20011" barStyle="light-content" />
+
+      <Box safeAreaTop backgroundColor="#e60014" />
+
+      <HStack bg='#e60014' px={1} py={3} justifyContent='space-between' alignItems='center'>
+        <HStack space={4} alignItems='center'>
+          <IconButton icon={<Icon size="sm" as={<AntDesign name='arrowleft' />} color="white" />} />
+        </HStack>
+        <HStack space={2}>
+          <IconButton icon={<Icon as={<MaterialIcons name='favorite-border' />} size='sm' color="white" />} />
+          <IconButton icon={<Icon as={<MaterialIcons name='share' />}
+            color="white" size='sm' />} />
+          <IconButton icon={<Icon as={<Fontisto name='shopping-basket' />} size='sm' color="white" />} />
+        </HStack>
+      </HStack>
+    </>
+    )
+  }
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+        <NativeBaseProvider>
+          <AppBar/>
+        </NativeBaseProvider>
   );
-}
+  }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+          container: {
+          flex: 1,
+    backgroundColor: '#e60014',
     alignItems: 'center',
     justifyContent: 'center',
   },
