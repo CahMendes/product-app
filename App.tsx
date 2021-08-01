ccf0938393369723e5eb7f3ea326d669aedf6904
CahMendes <cah.mendes35@gmail.com>
2021-08-01 18:26:24 -0300

    teste branch

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ function AppBar() {
     <>
       <StatusBar backgroundColor="#c20011" barStyle="light-content" />
 
-      <Box safeAreaTop backgroundColor="#e60014" />
+      <Box safeAreaTop backgroundColor="#e60014" /> 
 
       <HStack bg='#e60014' px={1} py={3} justifyContent='space-between' alignItems='center'>
         <HStack space={4} alignItems='center'>
